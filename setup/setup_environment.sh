f914e1613dc39153bde340a437ec9d79cc9c938a
Yohan Kim <rdg7739@hotmail.com>
2020-03-02 21:43:34 -0500

    Update script for ubuntu os system

--- setup/setup_environment.sh
+++ setup/setup_environment.sh
@@ -14,34 +14,34 @@
 
 echo Pre-requisities
 echo installing jq
-sudo yum -y install jq
+sudo apt-get -y install jq
 
 echo Updating AWS CLI to the latest version
 sudo pip install awscli --upgrade
 
 echo "Installing needed python libraries"
-sudo alternatives --set python /usr/bin/python3.6
+# sudo alternatives --set python /usr/bin/python3.6
 sudo pip install boto3
 sudo pip install requests
 
-echo "Updating YUM repos"
-sudo yum update -y
+echo "Updating apt-get repos"
+sudo apt-get update -y
 
 echo "Installing telnet and emacs"
-sudo yum install -y telnet
-sudo yum -y install emacs
+sudo apt-get install -y telnet
+sudo apt-get -y install emacs
 
 echo "Updating Docker"
-sudo yum update -y docker
+sudo apt-get update -y docker
 
 echo "Installing GIT and other tools"
-sudo yum install libtool-ltdl-devel -y
-sudo yum install git -y
+sudo apt-get install libtool-ltdl-devel -y
+sudo apt-get install git -y
 
 echo "Installing Docker Compose"
 sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod a+x /usr/local/bin/docker-compose
-sudo yum install libtool -y
+sudo apt-get install libtool -y
 
 echo "Fixing Bash Profile"
 rm ~/.bash_profile
@@ -54,7 +54,7 @@ fi
 # User specific environment and startup programs
 PATH=$PATH:$HOME/.local/bin:$HOME/bin
 # GOROOT is the location where Go package is installed on your system
-export GOROOT=/usr/lib/golang/
+export GOROOT=/usr/lib/go-1.9/
 # GOPATH is the location of your work directory
 export GOPATH=$HOME/go
 # PATH in order to access go binary system wide
@@ -66,19 +66,24 @@ source ~/.bash_profile
 echo "Checking versions"
 docker version
 sudo /usr/local/bin/docker-compose version
+
+echo "Installing go"
+# sudo add-apt-repository ppa:longsleep/golang-backports
+# sudo apt-get update
+# sudo apt-get install golang-go
 go version
 
 ## Setup Fabric client
 echo "Setting up Fabric Client"
 go get -u github.com/hyperledger/fabric-ca/cmd/...
-cd /home/ec2-user/go/src/github.com/hyperledger/fabric-ca
+cd /home/ubuntu/go/src/github.com/hyperledger/fabric-ca/
 make fabric-ca-client
-export PATH=$PATH:/home/ec2-user/go/src/github.com/hyperledger/fabric-ca/bin # Add this to your.bash_profile to preserve across sessions
-echo "export PATH=\$PATH:/home/ec2-user/go/src/github.com/hyperledger/fabric-ca/bin" >> ~/.bash_profile
+export PATH=$PATH:/home/ubuntu/go/src/github.com/hyperledger/fabric-ca/bin # Add this to your.bash_profile to preserve across sessions
+echo "export PATH=\$PATH:/home/ubuntu/go/src/github.com/hyperledger/fabric-ca/bin" >> ~/.bash_profile
 cd ~
 
 echo "Getting TLS Certificate for Managed Blockchain"
-aws s3 cp s3://us-east-1.managedblockchain/etc/managedblockchain-tls-chain.pem  /home/ec2-user/managedblockchain-tls-chain.pem
+aws s3 cp s3://us-east-1.managedblockchain/etc/managedblockchain-tls-chain.pem  /home/ubuntu/managedblockchain-tls-chain.pem
 
 echo "Checking out Fabric Samples"
 cd ~
@@ -126,8 +131,8 @@ services:
     command: /bin/bash
     volumes:
         - /var/run/:/host/var/run/
-        - /home/ec2-user/go/:/opt/gopath/
-        - /home/ec2-user:/opt/home
+        - /home/ubuntu/go/:/opt/gopath/
+        - /home/ubuntu:/opt/home
 EOF
 
 echo "Pulling needed docker images. "
@@ -139,7 +144,7 @@ docker-compose -f ~/docker-compose-cli.yaml up &
 sleep 3
 
 echo "Creating utils."
-BIN_DIRECTORY=/home/ec2-user/bin
+BIN_DIRECTORY=/home/ubuntu/bin
 if [ ! -d $BIN_DIRECTORY ]; then
   mkdir -p $BIN_DIRECTORY
 fi
@@ -165,5 +170,5 @@ echo "2) Docker CLI helper script."
 echo "=========================================================================="
 echo "Completed successfully. Please run:"
 echo "1) source ~/.bash_profile"
-echo "2) ~/environment/BanktransferBlockchainWorkshop/setup/setup_fabric_environment.py to finish setting up this environment."
+echo "2) ~/environment/bank-transfer-blockchain-reinvent2019-workshop/setup/setup_fabric_environment.py to finish setting up this environment."
 echo "=========================================================================="
